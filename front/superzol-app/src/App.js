b1c49d3ce91fb9cf3c82a9d06c562770e6bf6b3a
Adam.Ozeri <Adam.Ozeri@s.afeka.ac.il>
2024-05-17 12:10:50 +0300

    fixed indentations

--- front/superzol-app/src/App.js
+++ front/superzol-app/src/App.js
@@ -1,35 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
-import { GlobalStyles } from '@mui/system';
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {ThemeProvider, createTheme, CssBaseline} from '@mui/material';
+import {GlobalStyles} from '@mui/system';
 import Login from "./components/Login";
-import { AuthProvider } from "./contexts/AuthContext";
+import {AuthProvider} from "./contexts/AuthContext";
 import Register from "./components/Register";
 import Home from "./components/HomePage";
 
 const theme = createTheme();
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalStyles styles={{
-        body: {
-          backgroundImage: 'linear-gradient(45deg, #e66465 30%, #9198e5 90%)',
-        },
-      }}/>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            {/*  BASIC ROUTES AUTH  */}
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home/>} />
-            <Route path="/" element={<Navigate to="/login" />} />
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <GlobalStyles styles={{
+                body: {
+                    backgroundImage: 'linear-gradient(45deg, #e66465 30%, #9198e5 90%)',
+                },
+            }}/>
+            <AuthProvider>
+                <Router>
+                    <Routes>
+                        {/*  BASIC ROUTES AUTH  */}
+                        <Route path="/register" element={<Register/>}/>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/home" element={<Home/>}/>
+                        <Route path="/" element={<Navigate to="/login"/>}/>
 
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </ThemeProvider>
-  );
+                    </Routes>
+                </Router>
+            </AuthProvider>
+        </ThemeProvider>
+    );
 }
