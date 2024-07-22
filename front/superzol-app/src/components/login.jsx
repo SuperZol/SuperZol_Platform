918493b6a36766a5f039c945d2deec91717a54d5
netanel97 <netanelhabas@gmail.com>
2024-07-22 14:55:47 +0300

    Adding title and css styles

--- front/superzol-app/src/components/login.jsx
+++ front/superzol-app/src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
     Button,
     Grid,
@@ -15,7 +15,7 @@ import PersonIcon from "@mui/icons-material/Person";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import {useUser} from "../contexts/user-context";
+import { useUser } from "../contexts/user-context";
 import axios from "axios";
 import "../css/auth.css";
 
@@ -25,7 +25,7 @@ export const Login = () => {
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
 
-    const {currentUser, login, setError} = useUser();
+    const { currentUser, login, setError } = useUser();
     const getLocation = useCallback(async () => {
         if (!navigator.geolocation) {
             console.log("Geolocation is not supported by your browser");
@@ -57,7 +57,6 @@ export const Login = () => {
         }
     }, [currentUser, navigate, setError, getLocation]);
 
-
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -75,89 +74,94 @@ export const Login = () => {
     };
 
     return (
-        <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={2}
-            className="root"
-        >
-            <Paper
-                elevation={3}
-                className="paper"
+        <div className="root">
+               <Typography variant="h1" className="title">
+                SuperZol
+            </Typography>
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                className="grid-container"
             >
-                <Typography variant="h4" align="center" gutterBottom>
-                    Login
-                </Typography>
-                <Avatar className="avatar">
-                    <AccountCircleIcon />
-                </Avatar>
-                <form onSubmit={handleSubmit} className="form">
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            fullWidth
-                            margin="normal"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <EmailIcon/>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            fullWidth
-                            margin="normal"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <LockIcon/>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-                    <Grid item>
-                        <Button
-                            startIcon={<PersonIcon />}
-                            type="submit"
-                            disabled={loading}
-                            variant="contained"
-                            color="primary"
-                            className="button"
-                            size="large"
-                            fullWidth
-                        >
-                            {loading ? <CircularProgress size={24} /> : "Login"}
-                        </Button>
-                    </Grid>
-                    <Box mt={2} textAlign="center">
-                        <Typography variant="body2">
-                            Don't have an account?
-                            <Link
-                                to="/register"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3f51b5",
-                                    marginLeft: "5px",
+                <Paper
+                    elevation={3}
+                    className="paper"
+                >
+                    <Typography variant="h4" align="center" gutterBottom>
+                        Login
+                    </Typography>
+                    <Avatar className="avatar">
+                        <AccountCircleIcon />
+                    </Avatar>
+                    <form onSubmit={handleSubmit} className="form">
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                fullWidth
+                                margin="normal"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <EmailIcon/>
+                                        </InputAdornment>
+                                    ),
                                 }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                fullWidth
+                                margin="normal"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LockIcon/>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                startIcon={<PersonIcon />}
+                                type="submit"
+                                disabled={loading}
+                                variant="contained"
+                                color="primary"
+                                className="button"
+                                size="large"
+                                fullWidth
                             >
-                                Register
-                            </Link>
-                        </Typography>
-                    </Box>
-                </form>
-            </Paper>
-        </Grid>
+                                {loading ? <CircularProgress size={24} /> : "Login"}
+                            </Button>
+                        </Grid>
+                        <Box mt={2} textAlign="center">
+                            <Typography variant="body2">
+                                Don't have an account?
+                                <Link
+                                    to="/register"
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "#3f51b5",
+                                        marginLeft: "5px",
+                                    }}
+                                >
+                                    Register
+                                </Link>
+                            </Typography>
+                        </Box>
+                    </form>
+                </Paper>
+            </Grid>
+        </div>
     );
 };
--- front/superzol-app/src/components/register.jsx
+++ front/superzol-app/src/components/register.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
     Button,
     Grid,
@@ -10,8 +10,8 @@ import {
     Avatar,
 } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
-import {Link, useNavigate} from "react-router-dom";
-import {useUser} from "../contexts/user-context";
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../contexts/user-context";
 import PersonIcon from "@mui/icons-material/Person";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
@@ -25,7 +25,7 @@ export const Register = () => {
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [loading, setLoading] = useState(false);
-    const {register, setError,error} = useUser();
+    const { register, setError, error } = useUser();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -35,7 +35,6 @@ export const Register = () => {
             setError(passwordErrors.join(", "));
             return;
         }
-
 
         try {
             setError("");
@@ -49,108 +48,113 @@ export const Register = () => {
     };
 
     return (
-        <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={2}
-            className="root"
-        >
-            <Paper elevation={3} className="paper">
-                <Typography variant="h4" align="center" gutterBottom>
-                    Register
-                </Typography>
-                <Avatar className="avatar">
-                    <AccountCircleIcon />
-                </Avatar>
-                <form onSubmit={handleSubmit} className="form">
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            fullWidth
-                            margin="normal"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <EmailIcon />
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            fullWidth
-                            margin="normal"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <LockIcon/>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Confirm Password"
-                            type="password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            fullWidth
-                            margin="normal"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <LockIcon/>
-                                    </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </Grid>
-                    {error && (
-                        <Typography color="error" variant="body2" align="center" style={{ marginTop: '10px' }}>
-                            {error}
-                        </Typography>
-                    )}
-                    <Grid item>
-                        <Button
-                            startIcon={<PersonIcon />}
-                            type="submit"
-                            disabled={loading}
-                            variant="contained"
-                            color="primary"
-                            className="button"
-                            size="large"
-                            fullWidth
-                        >
-                            {loading ? <CircularProgress size={24} /> : "Register"}
-                        </Button>
-                    </Grid>
-                    <Box mt={2} textAlign="center">
-                        <Typography variant="body2">
-                            Already have an account?
-                            <Link
-                                to="/login"
-                                style={{
-                                    textDecoration: "none",
-                                    color: "#3f51b5",
-                                    marginLeft: "5px",
+        <div className="root">
+            <Typography variant="h1" className="title">
+                SuperZol
+            </Typography>
+            <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                className="grid-container"
+            >
+                <Paper elevation={3} className="paper">
+                    <Typography variant="h4" align="center" gutterBottom>
+                        Register
+                    </Typography>
+                    <Avatar className="avatar">
+                        <AccountCircleIcon />
+                    </Avatar>
+                    <form onSubmit={handleSubmit} className="form">
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                fullWidth
+                                margin="normal"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <EmailIcon />
+                                        </InputAdornment>
+                                    ),
                                 }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                fullWidth
+                                margin="normal"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LockIcon />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Confirm Password"
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                fullWidth
+                                margin="normal"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LockIcon />
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        </Grid>
+                        {error && (
+                            <Typography color="error" variant="body2" align="center" style={{ marginTop: '10px' }}>
+                                {error}
+                            </Typography>
+                        )}
+                        <Grid item>
+                            <Button
+                                startIcon={<PersonIcon />}
+                                type="submit"
+                                disabled={loading}
+                                variant="contained"
+                                color="primary"
+                                className="button"
+                                size="large"
+                                fullWidth
                             >
-                                Login
-                            </Link>
-                        </Typography>
-                    </Box>
-                </form>
-            </Paper>
-        </Grid>
+                                {loading ? <CircularProgress size={24} /> : "Register"}
+                            </Button>
+                        </Grid>
+                        <Box mt={2} textAlign="center">
+                            <Typography variant="body2">
+                                Already have an account?
+                                <Link
+                                    to="/login"
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "#3f51b5",
+                                        marginLeft: "5px",
+                                    }}
+                                >
+                                    Login
+                                </Link>
+                            </Typography>
+                        </Box>
+                    </form>
+                </Paper>
+            </Grid>
+        </div>
     );
 };
