ef602c5a4372adccf88b5452f1126d52af68da30
netanel97 <netanelhabas@gmail.com>
2024-07-26 10:26:32 +0300

    Creating a toolbar

---
+++ front/superzol-app/src/components/toolbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Box, Typography, Button} from "@mui/material";
+import {useNavigate, useLocation} from "react-router-dom"; // Import useLocation
+import '../css/toolbar.css';
+import {useUser} from '../contexts/user-context';
+
+const Toolbar = () => {
+    const navigate = useNavigate();
+    const location = useLocation(); // Get the current location
+    const {logout} = useUser();
+
+    const handleLogout = () => {
+        logout();
+        navigate('/login');
+    };
+
+    const handleNavigation = (path) => {
+        if (location.pathname !== path) {
+            navigate(path);
+        }
+    };
+
+    return (
+        <Box className="toolbar">
+            <Box>
+                <Button className="toolbar-button" onClick={handleLogout}>
+                    <Typography variant="body1">יציאה</Typography>
+                </Button>
+                <Button className="toolbar-button" onClick={() => handleNavigation('/user_preferences')}>
+                    <Typography variant="body1">הגדרות</Typography>
+                </Button>
+            </Box>
+            <Typography className="toolbar-title" onClick={() => handleNavigation('/home')}>
+                סופרזול
+            </Typography>
+        </Box>
+    );
+};
+
+export default Toolbar;
