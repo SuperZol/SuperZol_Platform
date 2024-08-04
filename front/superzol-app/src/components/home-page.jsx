cde0032929c1511bf77798472d998d6e30ac0ecf
netanel97 <netanelhabas@gmail.com>
2024-08-04 13:25:24 +0300

    Add loading animation but still need to fix the search animation

--- front/superzol-app/src/components/home-page.jsx
+++ front/superzol-app/src/components/home-page.jsx
@@ -8,6 +8,7 @@ import {useProduct} from "../contexts/product-context";
 import {ShoppingCart} from './shopping-cart';
 import Toolbar from "./toolbar";
 import {CategoriesModal} from "./categories-modal";
+import {ClipLoader} from "react-spinners";
 
 
 export const Home = () => {
@@ -29,7 +30,7 @@ export const Home = () => {
     const [category, setCategory] = useState("");
     const [searchName, setSearchName] = useState("");
     const [isModalOpen, setModalOpen] = useState(false);
-
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         if (!currentUser) {
@@ -39,21 +40,29 @@ export const Home = () => {
     }, [currentUser, navigate, setError]);
 
     useEffect(() => {
-        if (isSearchByName && isSearchByCategory) {
-            searchProductsByNameAndCategory(searchName, category, page, pageSize)
-        } else if (isSearchByName) {
-            searchProductsByName(searchName, page, pageSize);
-        } else if (isSearchByCategory) {
-            searchProductsByCategory(category, page, pageSize);
-        } else {
-            getAllProducts(page, pageSize)
-        }
+        const fetchProducts = async () => {
+            setLoading(true);
+            try {
+                if (isSearchByName && isSearchByCategory) {
+                    await searchProductsByNameAndCategory(searchName, category, page, pageSize);
+                } else if (isSearchByName) {
+                    setLoading(false);
+                    await searchProductsByName(searchName, page, pageSize);
+                } else if (isSearchByCategory) {
+                    await searchProductsByCategory(category, page, pageSize);
+                } else {
+                    await getAllProducts(page, pageSize);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchProducts();
     }, [page, pageSize, isSearchByName, isSearchByCategory, category, searchName, getAllProducts, searchProductsByCategory, searchProductsByName, searchProductsByNameAndCategory]);
 
     useEffect(() => {
         setPage(1);
     }, [isSearchByName, isSearchByCategory]);
-
 
     const handleSearchByName = (productName) => {
         if (productName !== "") {
@@ -87,7 +96,7 @@ export const Home = () => {
 
     const removeFromCart = (productId) => {
         setShoppingList((prev) => {
-            const newList = {...prev};
+            const newList = { ...prev };
             if (newList[productId]) {
                 delete newList[productId];
             }
@@ -116,38 +125,46 @@ export const Home = () => {
         setIsSearchByCategory(false);
     }
 
-
-    return (<>
-        <Toolbar onLogout={logout}/>
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            minHeight="100vh"
-            sx={{textAlign: "center"}}
-            marginTop="70px"
-        >
-            <SearchBar onSearch={handleSearchByName} onCategoriesClick={handleCategoriesClick}/>
-            {isSearchByCategory && (
-                <Button onClick={() => disableCategory()}>{category} x</Button>
+    return (
+        <>
+            <Toolbar onLogout={logout} />
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                minHeight="100vh"
+                sx={{ textAlign: "center" }}
+                marginTop="70px"
+            >
+                {loading ? (
+                    <ClipLoader size={150} color={"#123abc"} loading={loading} />
+                ) : (
+                    <>
+                        <SearchBar onSearch={handleSearchByName} onCategoriesClick={handleCategoriesClick} />
+                        {isSearchByCategory && (
+                            <Button onClick={() => disableCategory()}>{category} x</Button>
+                        )}
+                        <CategoriesModal isOpen={isModalOpen} onClose={handleCloseModal} filterCategory={filterCategory}></CategoriesModal>
+                        <ProductList products={products} addToCart={addToCart} />
+                        <Button onClick={() => handleNextPage()}>הבא</Button>
+                        <Button onClick={() => handlePrevPage()}>הקודם</Button>
+                    </>
+                )}
+                {isSidebarOpen && (
+                    <ShoppingCart shoppingList={shoppingList} setShoppingList={setShoppingList} removeFromCart={removeFromCart} setSidebarOpen={setSidebarOpen} />
+                )}
+            </Box>
+            {!loading && (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => setSidebarOpen(true)}
+                    sx={{ mt: 3 }}
+                >
+                    Cart
+                </Button>
             )}
-            <CategoriesModal isOpen={isModalOpen} onClose={handleCloseModal}
-                             filterCategory={filterCategory}></CategoriesModal>
-            <ProductList products={products} addToCart={addToCart}/>
-            <Button onClick={() => handleNextPage()}>הבא</Button>
-            <Button onClick={() => handlePrevPage()}>הקודם</Button>
-            {isSidebarOpen &&
-                <ShoppingCart shoppingList={shoppingList} setShoppingList={setShoppingList}
-                              removeFromCart={removeFromCart} setSidebarOpen={setSidebarOpen}/>}
-        </Box>
-        <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setSidebarOpen(true)}
-            sx={{mt: 3}}
-        >
-            Cart
-        </Button>
-    </>)
+        </>
+    );
 };
